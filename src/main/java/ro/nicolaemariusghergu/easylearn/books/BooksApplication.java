9583e81e4ed83490cdc0ded223af947776a7b23a
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-05-26 14:54:51 +0300

    Fix build

--- src/main/java/ro/nicolaemariusghergu/easylearn/books/BooksApplication.java
+++ src/main/java/ro/nicolaemariusghergu/easylearn/books/BooksApplication.java
@@ -2,8 +2,10 @@ package ro.nicolaemariusghergu.easylearn.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ro.nicolaemariusghergu.easylearn.books.repository")
 public class BooksApplication {
 
     public static void main(String[] args) {
